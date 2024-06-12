936c5dace28b36e238aa18dab37c7c6414ec164c
FGHTYGHVKRTKRK <thanhtuoi852123@gmail.com>
2024-06-12 14:20:44 +0700

    them config vao du an

---
+++ src/main/java/com/example/WebBanHang/SecurityConfig.java
@@ -0,0 +1,81 @@
+package com.example.WebBanHang;
+
+import com.example.WebBanHang.service.UserService;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserService userService;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserService();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        var auth = new DaoAuthenticationProvider(); // Tạo nhà cung cấp xác thực.
+        auth.setUserDetailsService(userDetailsService()); // Thiết lập dịch vụ chi tiết người dùng.
+        auth.setPasswordEncoder(passwordEncoder()); // Thiết lập cơ chế mã hóa mật khẩu.
+        return auth; // Trả về nhà cung cấp xác thực.
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(@NotNull HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**", "/js/**", "/", "/oauth/**", "/register", "/error",
+                                "/products", "/cart", "/cart/**")
+                        .permitAll()
+                        .requestMatchers("/products/edit/**", "/products/add", "/products/delete")
+                        .hasAnyAuthority("ADMIN")
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login") // Trang chuyển hướng sau khi đăng xuất.
+                        .deleteCookies("JSESSIONID") // Xóa cookie.
+                        .invalidateHttpSession(true) // Hủy phiên làm việc.
+                        .clearAuthentication(true) // Xóa xác thực.
+                        .permitAll()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login") // Trang đăng nhập.
+                        .loginProcessingUrl("/login") // URL xử lý đăng nhập.
+                        .defaultSuccessUrl("/products") // Trang sau đăng nhập thành công.
+                        .failureUrl("/login?error") // Trang đăng nhập thất bại.
+                        .permitAll()
+                )
+                .rememberMe(rememberMe -> rememberMe
+                        .key("hutech")
+                        .rememberMeCookieName("hutech")
+                        .tokenValiditySeconds(24 * 60 * 60) // Thời gian nhớ đăng nhập.
+                        .userDetailsService(userDetailsService())
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .accessDeniedPage("/403") // Trang báo lỗi khi truy cập không được phép.
+                )
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .maximumSessions(1) // Giới hạn số phiên đăng nhập.
+                        .expiredUrl("/login") // Trang khi phiên hết hạn.
+                )
+                .httpBasic(httpBasic -> httpBasic
+                        .realmName("hutech") // Tên miền cho xác thực cơ bản.
+                )
+                .build(); // Xây dựng và trả về chuỗi lọc bảo mật.
+    }
+}
